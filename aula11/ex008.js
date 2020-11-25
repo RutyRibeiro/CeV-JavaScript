090371755f1269292d9d9912f08d9e2aecc33b9c
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-11-25 16:57:20 -0300

    aula 11 ex 08

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+
+console.log(`a velocidade do seu carro é ${vel}KM/h`)
+
+if(vel > 60){
+    console.log('Passou a velocidade permitida. Multado')
+}
+console.log('Use cinto de segurança')
