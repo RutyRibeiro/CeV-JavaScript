0e70d3152d8851fae259eedd0944def0d93ec9c7
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-11-27 15:18:33 -0300

    finalização do execicio 14

--- exercicio014/script.js
+++ exercicio014/script.js
@@ -2,20 +2,24 @@ function load(){
     var hor = new Date()
     var hora = document.getElementById('hora')
     var msg = document.getElementById('img')
-    var body = document.getElementsByTagName('body')
-    hora.innerHTML=`<p>São ${hor.getHours()} horas </p>` 
-
+    var body = document.getElementById('body')
+   
     if(hor.getHours()>= 0 && hor.getHours()<12){
-        msg.innerHTML=`<p>Bom dia</p>` 
-        body.style.background='black'
+        hora.innerHTML=`<p>São ${hor.getHours()} horas da manhã</p>` 
+        msg.innerHTML='<img src="./imagens/manha.png" alt="">'
+        body.style.background='#ddd8b0'
     }
     else if(hor.getHours()>= 12 && hor.getHours()<18){
-        msg.innerHTML=`<p>Boa Tarde</p>`
-        body.style.background='black'
+        hora.innerHTML=`<p>São ${hor.getHours()} horas da tarde</p>` 
+        msg.innerHTML='<img src="./imagens/tarde.png" alt="">'
+        body.style.background='#f39419'
 
     }
     else{
-        msg.innerHTML=`<p>Boa noite</p>`
+        hora.innerHTML=`<p>São ${hor.getHours()} horas da noite </p>` 
+        msg.innerHTML='<img src="./imagens/tarde.png" alt="">'
+        body.style.background='#0e5f8d'
+
     }
 
 }
